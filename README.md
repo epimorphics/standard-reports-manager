09cefd835c92388c8aab6faac8e63c6cbabcbdec
der <dave@epimorphics.com>
2015-11-17 17:16:54 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Standard Reports Manager
+
+Reports manager for constructing batch reports.
+
+Implements both report submission/status tracking web service and backend report processor
---
+++ src/main/java/run/SAPIRun.java
@@ -0,0 +1,44 @@
+/*                                                                                                                            
+    LICENCE summary to go here.                                                                                        
+    
+    (c) Copyright 2014 Epimorphics Limited
+*/
+
+package run;
+
+import java.io.File;
+import java.net.URL;
+
+import org.apache.catalina.startup.Tomcat;
+
+public class SAPIRun {
+
+    public static void main(String[] args) throws Exception {
+        String root = "src/main/webapp";
+
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+
+        tomcat.setBaseDir(".");
+
+        String contextPath = "/dms";
+
+        File rootF = new File(root);
+        if (!rootF.exists()) {
+            rootF = new File(".");
+        }
+        if (!rootF.exists()) {
+            System.err.println("Can't find root app: " + root);
+            System.exit(1);
+        }
+
+        org.apache.catalina.Context context = tomcat.addWebapp(contextPath,  rootF.getAbsolutePath());
+        context.setConfigFile(new URL("file:src/main/webapp/META-INF/context.xml"));
+
+        tomcat.start();
+        tomcat.getServer().await();
+
+      }
+
+
+}
