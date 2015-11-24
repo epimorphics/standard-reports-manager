6020eeb92575a5c0647d709dd40b04f4cb6f3990
der <dave@epimorphics.com>
2015-11-24 17:14:12 +0000

    Starting on latestMonthAvailable

--- src/main/java/com/epimorphics/standardReports/webapi/LatestMonthAvailable.java
+++ src/main/java/com/epimorphics/standardReports/webapi/LatestMonthAvailable.java
@@ -9,18 +9,48 @@
 
 package com.epimorphics.standardReports.webapi;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Path("latest-month-available")
 public class LatestMonthAvailable extends SREndpointBase {
+    static Logger log = LoggerFactory.getLogger( LatestMonthAvailable.class );
+    
+    protected static String latestMonth;
+    protected long lastCheck = 0;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getLatestMonthAvailable() {
+        if (latestMonth == null || (System.currentTimeMillis() - lastCheck) > 12 * 60 * 60 * 1000) {
+            updateDate();
+        }
+        return latestMonth;
+    }
+    
+    public synchronized void updateDate() {
+        log.info("Updating latest month available");
+        LocalDate probe = LocalDate.now();
+        probe = LocalDate.of( probe.getYear(), probe.getMonth(), 1);
+        while (! isPresent(probe) ) {
+            probe = probe.minus(1,ChronoUnit.MONTHS); 
+            log.info("Probing for: " + probe.format(DateTimeFormatter.ofPattern("yyyy-MM")));
+        }
+        latestMonth = probe.format(DateTimeFormatter.ofPattern("yyyy-MM"));
+        log.info("Latest month available = " + latestMonth);
+    }
+
+    public boolean isPresent(LocalDate probe) {
         // TODO implement
-        return "2015-09";
+        return true;
     }
 }
