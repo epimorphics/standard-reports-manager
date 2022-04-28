ee6cdc698ee5dd410d96fcb6ed71362297cb765d
der <dave@epimorphics.com>
2022-04-28 18:28:42 +0100

    Improve logging format
    - timestamp field now ts in compliant format
    - request/response logs now contain an explicit path to enable filtering
    - includes request_id field
    - uses incoming x-request-id field if present else generates transaction count
    
    Addresses: #12 and part of  epimorphics/hmlr-ansible-deployment#76

---
+++ src/main/java/com/epimorphics/standardReports/webapi/LogRequestFilter.java
@@ -0,0 +1,81 @@
+package com.epimorphics.standardReports.webapi;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.Response.Status;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.epimorphics.appbase.webapi.WebApiException;
+import com.epimorphics.util.NameUtils;
+import org.slf4j.MDC;
+
+/**
+ * A Filter that can be added to filter chain to log all incoming requests and
+ * the corresponding response (with response code and execution time). Assigns a
+ * simple request number to each request and includes that in the response headers
+ * for diagnosis. Not robust against restarts but easier to work with than UUIDs.
+ */
+public class LogRequestFilter implements Filter {
+    public static final String TRANSACTION_ATTRIBUTE = "transaction";
+    public static final String START_TIME_ATTRIBUTE  = "startTime";
+    public static final String RESPONSE_ID_HEADER  = "X-Response-ID";
+    public static final String REQUEST_ID_HEADER  = "X-Request-ID";
+
+    static final Logger log = LoggerFactory.getLogger( LogRequestFilter.class );
+
+    protected static AtomicLong transactionCount = new AtomicLong(0);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest)request;
+        HttpServletResponse httpResponse = (HttpServletResponse)response;
+        String path = httpRequest.getRequestURI();
+        String query = httpRequest.getQueryString();
+        String requestID = httpRequest.getHeader(REQUEST_ID_HEADER);
+        if (requestID == null || requestID.isEmpty()) {
+            requestID = Long.toString(transactionCount.incrementAndGet());
+        }
+        MDC.put("request_id", requestID);
+        long start = System.currentTimeMillis();
+
+        String fullpath = path + (query == null ? "" : ("?" + query));
+        MDC.put("path", fullpath);
+        log.info( String.format("Request  [%s] : %s", requestID, fullpath) );
+        httpResponse.addHeader(RESPONSE_ID_HEADER, requestID);
+        chain.doFilter(request, response);
+        log.info( String.format("Response [%s] : %d (%s)", requestID, httpResponse.getStatus(),
+                NameUtils.formatDuration(System.currentTimeMillis() - start)) );
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    public static WebApiException badRequestException(String message) {
+        log.warn("Bad request: " + message);
+        return new WebApiException(Status.BAD_REQUEST, message);
+    }
+
+    public static WebApiException serverErrorException(String message) {
+        log.warn("Server error: " + message);
+        return new WebApiException(Status.INTERNAL_SERVER_ERROR, message);
+    }
+
+}
+
